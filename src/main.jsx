ab2eee83ab64e2c0034939da7d0b0d9542bba9e9
mbennell <95391380+mbennell@users.noreply.github.com>
2025-05-02 12:44:41 +1000

    Initial upload of PromptMeUp
    Initial upload of PromptMeUp MVP (ShadCN + Tailwind + Vite)

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import PromptLibrary from './PromptLibrary';
+ReactDOM.createRoot(document.getElementById('root')).render(<PromptLibrary />);
